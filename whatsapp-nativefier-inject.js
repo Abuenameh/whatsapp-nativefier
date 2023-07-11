29d3bc5b18aea0f70d6b0f7004f3c708c141b864
Abuenameh <abuenameh@gmail.com>
2023-07-11 10:25:28 -0400

    Add unread count

--- whatsapp-nativefier-inject.js
+++ whatsapp-nativefier-inject.js
@@ -45,6 +45,57 @@ for(var number = 0; number < NUMBER_OF_INVOCATIONS; number++) {
     }, number * TIME_BETWEEN_INVOCATIONS);
 }
 
+const getMessages = () => {
+    if(!window.dbCache) {
+        const dbsPromise = indexedDB.databases()
+        dbsPromise.then((databases) => {
+            for(let index in databases) {
+                //Wait for model-storage db to be available before calling indexedDB.open(). This is to make sure whatsapp created the model-storage DB
+                if(databases[index].name === "model-storage") {
+                    const request = window.indexedDB.open("model-storage");
+                    request.onsuccess = () => {
+                        window.dbCache = request.result;
+                        //This will be called when db.delete is triggered, we need to close and set dbCache to null to trigger lookup again
+                        window.dbCache.onversionchange = () => {
+                            window.dbCache.close()
+                            window.dbCache = null
+                        };
+                    }
+                    request.addEventListener('error', () => {
+                        console.error("Opening model-storage database failed:", event);
+                    })
+                }
+            }
+        })
+    } else {
+        let unreadCount = 0;
+        let unreadMutedCount = 0;
+
+        const txn = window.dbCache.transaction('chat', 'readonly');
+        const store = txn.objectStore('chat');
+        const query = store.getAll();
+        query.onsuccess = (event) => {
+            for (const chat of event.target.result) {
+                if (chat.unreadCount > 0) {
+                    if (chat.muteExpiration === 0 && !chat.archive) {
+                        unreadCount += chat.unreadCount;
+                    } else {
+                        unreadMutedCount += chat.unreadCount;
+                    }
+                }
+            }
+
+            document.title = "(" + unreadCount.toString() + ") WhatsApp";
+        };
+
+        query.addEventListener('error', (event) => {
+            console.error("Loading data from database failed:", event);
+        })
+    }
+}
+
+setInterval(getMessages, 1000);
+
 const electron = require("electron");
 const request = {
   property: "spellCheckerEnabled",
